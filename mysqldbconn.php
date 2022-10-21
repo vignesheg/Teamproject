3e8a22cf8bf765b5cc3235a5ddd495dcf9d4dce1
vignesheg <vignesheg10@.com>
2022-10-21 14:40:49 +0530

    sixth

--- mysqldbconn.php
+++ mysqldbconn.php
@@ -11,7 +11,7 @@ $db = "d2d1f5co2r9tbn";
 $conn = pg_connect("host=ec2-54-82-205-3.compute-1.amazonaws.com port=5432 dbname=d2d1f5co2r9tbn user=
 dmnbxmdqdnxeah password=
 7db3e696aded357a7627f4d476c1ccc2a8a2b5866774ef6ca69c963a9f6beb2a");
-if($conn){
+if(!$conn){
     die('connection failed:'.mysqli_connect_error());
 }
 ?>
