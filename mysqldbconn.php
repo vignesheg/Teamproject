8c97d0e4a26d083f8797a5c35c40dba35fe79e60
vignesheg <vignesheg10@.com>
2022-10-21 14:43:09 +0530

    jk

--- mysqldbconn.php
+++ mysqldbconn.php
@@ -12,6 +12,7 @@ $conn = pg_connect("host=ec2-54-82-205-3.compute-1.amazonaws.com port=5432 dbnam
 dmnbxmdqdnxeah password=
 7db3e696aded357a7627f4d476c1ccc2a8a2b5866774ef6ca69c963a9f6beb2a");
 if($conn){
-    die('connection failed:'.mysqli_connect_error());
-}
+   echo "connected";}else{
+    echo "not connected";
+   }
 ?>
