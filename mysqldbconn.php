1f641a75cf9b3716acba981edec149c027272770
vignesheg <vignesheg10@.com>
2022-10-21 13:56:59 +0530

    four

--- mysqldbconn.php
+++ mysqldbconn.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $server='ec2-54-82-205-3.compute-1.amazonaws.com';
 $usr='dmnbxmdqdnxeah';
 $pass='7db3e696aded357a7627f4d476c1ccc2a8a2b5866774ef6ca69c963a9f6beb2a';
