7486e1d45bf78247f21405f60cd1643fbbc3e5f7
vignesheg <vignesheg10@.com>
2022-10-22 19:01:09 +0530

    update

--- userslist.php
+++ userslist.php
@@ -12,11 +12,20 @@ $result = pg_query($conn,$sql);
 
 <html>
 
-<head></head>
+<head>
+
+<title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
 
 <body>
-    <table>
-        <tr>
+    <table class="table">
+        <tr class="info">
             <th>Name</th>
             <th>Display Name</th>
             <th>Email</th>
