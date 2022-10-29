2e0128e129770eacaff9bbb28ac69cabfe031a2b
vignesheg <vignesheg10@.com>
2022-10-29 15:02:03 +0530

    update

--- createtable.php
+++ createtable.php
@@ -4,10 +4,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require "mysqldbconn.php";
 
-$sql = "ALTER TABLE users
-ADD status varchar(5);";
 
-$result = pg_query($conn,$sql);
+$sql1 = "ALTER TABLE users
+DROP COLUMN status;";
+
+$sql = "ALTER TABLE users
+ADD status varchar(200);";
+
+$result = pg_query($conn,$sql1);
 if($result){
   echo "Table users created ";}else{
   echo "table users not created";}
