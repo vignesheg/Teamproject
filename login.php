171836cc7c8ecd94f6e151b41c0e65ba4e78bba5
vignesheg <vignesheg10@.com>
2022-11-03 13:58:20 +0530

    update

--- login.php
+++ login.php
@@ -188,7 +188,7 @@ if($num == 0){}else{
                 <h4 class="pt-3 pb-4 text-center">Have an account?</h4>
                 <form>
                     <div class="text-center">
-                        <input type="text" name="gmail" value="<?php if(isset($_COOKIE['emailcookie'])){ echo $email;}else{ echo $username;} ?> " class="text-white name border border-secondary rounded-pill ps-3 pe-5"
+                        <input type="text" name="gmail" value="<?php if(isset($_COOKIE['emailcookie'])){ echo $email;} ?> " class="text-white name border border-secondary rounded-pill ps-3 pe-5"
                             style="padding-top: 12px;padding-bottom:12px;" placeholder="Email or username"><br><br>
                         <input type="password" value="" name="password" class="name border border-secondary rounded-pill ps-3 pe-5"
                             style="padding-top: 12px;padding-bottom:12px;" placeholder="Password"><br><br>
