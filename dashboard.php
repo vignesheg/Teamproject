be9f0d30b368864e04f9bec47028de568eb7114e
vignesheg <vignesheg10@.com>
2022-11-10 21:23:03 +0530

    update

--- dashboard.php
+++ dashboard.php
@@ -154,22 +154,13 @@
         </nav>
     </div>
 
-
-    <div id="vrmn" style="width:230px;" class="vrmn position-fixed h-100 shadow bg-dark">
-        <div class="container-fluid">
-            <ul class="navbar-nav align-baseline" style="margin-top:6rem ;">
-                <li class="nav-item pb-2">
-                    <a href="#" class="hovlink nav-link text-light"><i class="fa-solid fa-gauge-simple"
-                            style="font-size:18px;padding-right:5px;"></i>Dashboard</a> </li>
-                <li class="nav-item pb-2">
-                    <a href="#" class="hovlink nav-link text-light"><i class="fa-solid fa-users"
-                            style="font-size:18px;padding-right:5px;"></i>users list</a>
-                </li>
-            </ul>
-
-        </div>
-
-    </div>
+ <?php //sidebar ?>
+   <?php
+   
+   require ('sidebar.php');
+   
+   
+   ?>
     <div class="iframe">
     <iframe src="userslist.php" scrolling="no" class="position-absolute h-100"  style="left: 12.5%;top:10%;width: 87%;"   frameborder="0"></iframe>
 </div>
---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<div id="vrmn" style="width:230px;" class="vrmn position-fixed h-100 shadow bg-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav align-baseline" style="margin-top:6rem ;">
+                <li class="nav-item pb-2">
+                    <a href="#" class="hovlink nav-link text-light"><i class="fa-solid fa-gauge-simple"
+                            style="font-size:18px;padding-right:5px;"></i>Dashboard</a> </li>
+                <li class="nav-item pb-2">
+                    <a href="#" class="hovlink nav-link text-light"><i class="fa-solid fa-users"
+                            style="font-size:18px;padding-right:5px;"></i>users list</a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
