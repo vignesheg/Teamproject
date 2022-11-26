37f2120f371bd06052d137a06925e606c6d289fc
vignesheg <108560341+vignesheg@users.noreply.github.com>
2022-11-26 19:09:22 +0530

    Update register.php

--- register.php
+++ register.php
@@ -79,7 +79,7 @@ if(isset($_POST['register_button'])){
 
     $passwordhashed = password_hash($password,PASSWORD_DEFAULT);
     $insertusers = "INSERT INTO usersregular (id, name, email,gender,password,signup_date,profile_pic,num_post,num_likes,user_closed,friend_arrat) 
-    VALUES ('', '$name', '$email','$gender','$passwordhashed','$date','$profile_pic',0,0,'no','0')";
+    VALUES ('', '$name', '$email','$gender','$passwordhashed','$date','$profile_pic','0','0','no','0')";
     $run2 = pg_query($conn,$insertusers);
     if($run2 == true){
       echo '<script>window.location.replace("login.php");</script>';
