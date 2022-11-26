684fe89f13f5e9eaf1d2c312c54cc32efe3f5df2
vignesheg <vignesheg10@.com>
2022-11-26 17:29:41 +0530

    update

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
 
-require 'pgdbconn.php';
+require 'mysqldbconn.php';
 
 //Declaring variables to prevent error
 $name = '';
